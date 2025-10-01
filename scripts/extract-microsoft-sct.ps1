74e161e2907e06a0a34b5cfd93df28533ac18c0f
Maggie Machuca <magalymachuca@outlook.com>
2025-09-30 22:09:42 -0700

    Create extract-microsoft-sct.ps1

---
+++ scripts/extract-microsoft-sct.ps1
@@ -0,0 +1,6 @@
+# Create folders for each extracted component
+Expand-Archive -Path "C:\baselines\SCT\Windows Server 2022 Security Baseline.zip" -DestinationPath "C:\baselines\SCT\Windows Server 2022 Security Baseline" -Force
+
+Expand-Archive -Path "C:\baselines\SCT\LGPO.zip" -DestinationPath "C:\baselines\SCT\LGPO" -Force
+
+Expand-Archive -Path "C:\baselines\SCT\PolicyAnalyzer.zip" -DestinationPath "C:\baselines\SCT\PolicyAnalyzer" -Force
